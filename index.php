71cafc7b90fea79f006f04d6a65d128a7eeee0bf
orlandolanaaa <orlandolana09@gmail.com>
2018-09-23 15:02:40 +0700

    a

--- index.php
+++ index.php
@@ -9,12 +9,8 @@
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-<<<<<<< HEAD
-	<link rel="stylesheet" type="text/css" href="css/home.css">
-=======
 	<link rel="stylesheet" type="text/css" href="css/index.css">
 
->>>>>>> 99c222ed8b69e926917fcc0cfec8947ec5c681b6
 </head> 
 <body>
 <div>
